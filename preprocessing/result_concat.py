3f3ea85920ed88e87b7d51881148936697dd1a32
MelProbe <melprobe@yonsei.ac.kr>
2020-02-20 16:32:01 +0900

    이진화 + edge

--- preprocessing/result_concat.py
+++ preprocessing/result_concat.py
@@ -15,21 +15,37 @@ def make_folder(folder_path):
 def make_concat_img(img_path, img, gt, seg):
     seg_ori = seg.copy()
 
-    seg[:,:,0] = 0
-    seg[:,:,1] = 0
-    seg[:,:,2] = seg[:,:,2] / 2
+    seg_ori_color = img.copy()
+    seg_ori_color[:,:,0] = seg_ori
+    seg_ori_color[:,:,1] = seg_ori
+    seg_ori_color[:,:,2] = seg_ori
 
-    seg_sum = cv2.add(img.copy(),seg.copy())
-    seg_concat_img = cv2.hconcat([img,seg_sum,seg_ori])
+    seg_edge = cv2.Canny(seg, 50, 150)
 
+    seg_color = img.copy()
+    seg_color[:,:,0] = 0
+    seg_color[:,:,1] = 0
+    seg_color[:,:,2] = seg_edge
+
+    seg_sum = cv2.add(img.copy(),seg_color.copy())
+    seg_concat_img = cv2.hconcat([img,seg_sum,seg_ori_color])
+    
     gt_ori = gt.copy()
 
-    gt[:,:,0] = 0
-    gt[:,:,1] = 0
-    gt[:,:,2] = gt[:,:,2] / 2
+    gt_ori_color = img.copy()
+    gt_ori_color[:,:,0] = gt_ori
+    gt_ori_color[:,:,1] = gt_ori
+    gt_ori_color[:,:,2] = gt_ori
 
-    gt_sum = cv2.add(img.copy(),gt.copy())
-    gt_concat_img = cv2.hconcat([img,gt_sum,gt_ori])
+    gt_edge = cv2.Canny(gt, 50, 150)
+
+    gt_color = img.copy()
+    gt_color[:,:,0] = 0
+    gt_color[:,:,1] = 0
+    gt_color[:,:,2] = gt_edge
+
+    gt_sum = cv2.add(img.copy(),gt_color.copy())
+    gt_concat_img = cv2.hconcat([img,gt_sum,gt_ori_color])
 
     concat_img = cv2.vconcat([gt_concat_img,seg_concat_img])
 
@@ -41,9 +57,13 @@ seg_list = sorted(glob.glob('./result/SEG*.jpg'))
 img_list = sorted(glob.glob('./result/IMAGE*.jpg'))
 
 for index, img_path in enumerate(img_list):
-    gt = cv2.imread(gt_list[index], cv2.IMREAD_COLOR)
-    seg = cv2.imread(seg_list[index], cv2.IMREAD_COLOR)
+    gt = cv2.imread(gt_list[index], cv2.IMREAD_GRAYSCALE)
+    seg = cv2.imread(seg_list[index], cv2.IMREAD_GRAYSCALE)
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
+    ret, gt = cv2.threshold(gt,127,255,cv2.THRESH_BINARY)
+    ret, seg = cv2.threshold(seg,127,255,cv2.THRESH_BINARY)
+
+    #make_concat_img(img_path, img, gt, seg)
     t_concat = threading.Thread(target=make_concat_img, args=(img_path, img, gt, seg))
     t_concat.start()
